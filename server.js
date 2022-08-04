e29261e8339965cc5e237d68c2aab3754221fa69
= <=>
2022-08-04 23:09:22 +0800

    [Refactor] version two, use event to handle data change

--- server.js
+++ server.js
@@ -1,38 +1,31 @@
 const express = require('express');
+const events = require('events');
+const { resolve } = require('path');
+const { rejects } = require('assert');
+const eventEmitter = new events.EventEmitter();
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
 
-const maxTimeout = 5;
 let fakeData = []
 
 app.get("/data", async (req, res)=>{
-    let timeout = 0;
-    let previousData = [...fakeData]
-    let promiseData = await new Promise((resolve, reject)=>{
-        let interval = setInterval(()=>{
-            console.log(timeout)
-            if(timeout >= maxTimeout){
-                clearInterval(interval);
-                resolve(fakeData);
-            }
-            console.log(fakeData, previousData)
-            let hasChange = fakeData.some((element, index)=>{
-                return element !== fakeData[index]
-            })
-            if(hasChange || fakeData.length !== previousData.length){
-                clearInterval(interval);
-                resolve(fakeData)
-            }
-            timeout += 1;
-        },1000)
+    await new Promise((resolve, reject)=>{
+        let timeout = setTimeout(()=>{
+            console.log("has wait for 5 sec, no data changed")
+            resolve()
+        }, 5000)
+        eventEmitter.on('Data change', ()=>{
+            clearTimeout(timeout);
+            resolve()
+        })
     })
     console.log("out")
-    if(promiseData.length === 0){
+    if(fakeData.length === 0){
         return res.status(200).json({data: null});
     }
-    return res.status(200).json({data: promiseData})
+    return res.status(200).json({data: fakeData})
 
 })
 
@@ -40,13 +33,14 @@ app.post("/data", (req, res)=>{
     let {data} = req.body;
     fakeData.push(data)
     const inboundIP = req.header('x-forwarded-for') || req.socket.remoteAddress;
+    eventEmitter.emit("Data change")
     return res.send(`${inboundIP} says ${data}`);
 })
 
 // 發現奇特的點： RESTful 設計下的 delete 會等到 get 結束才會執行
 app.delete("/Deletedata", (req, res)=>{
     fakeData = [];
-    console.log("in")
+    eventEmitter.emit("Data change")
     return res.send("Deleted")
 })
 
